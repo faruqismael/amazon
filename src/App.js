30e1555cb696b3a5cdd43b81d73d818c48f09cf3
faruqismael <farusmith5@gmail.com>
2021-07-03 10:42:26 +0300

    building header

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import "./App.css";
+import Header from "./Header";
+
+const App = () => {
+  return (
+    <div className="app">
+      {/* header */}
+      <Header />
+
+      {/* home */}
+    </div>
+  );
+};
+
+export default App;
---
+++ src/Header.jsx
@@ -0,0 +1,39 @@
+import "./styles/header.css";
+import amazon from "./amazon.png";
+import SearchIcon from "@material-ui/icons/Search";
+import ShoppingBasketIcon from "@material-ui/icons/ShoppingBasket";
+
+const Header = () => {
+  return (
+    <div className="header">
+      <img src={amazon} alt="" className="header__logo" />
+
+      <div className="header__search">
+        <input type="text" className="header__searchInput" />
+        <SearchIcon className="header__searchIcon" />
+      </div>
+
+      <div className="header__nav">
+        <div className="header__option">
+          <span className="header__optionLineOne">Hello Guest</span>
+          <span className="header__optionLineTwo">Sign In</span>
+        </div>
+        <div className="header__option">
+          <span className="header__optionLineOne">Returns</span>
+          <span className="header__optionLineTwo">& Orders</span>
+        </div>
+        <div className="header__option">
+          <span className="header__optionLineOne">Your</span>
+          <span className="header__optionLineTwo">Prime</span>
+        </div>
+
+        <div className="header__optionBasket">
+          <ShoppingBasketIcon />
+          <span className="header__optionLineTwo header__basketCount">0 </span>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Header;
